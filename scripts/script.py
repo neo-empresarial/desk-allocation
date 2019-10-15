06750c2065bc47899555754fd7fb76476a027a97
José Ribeiro <ribeirojose@me.com>
2019-10-14 23:25:04 -0300

    Remove sensitive info

--- scripts/script.py
+++ scripts/script.py
@@ -7,6 +7,17 @@ list_ = [a[a.day == x] for x in a.day.unique().tolist()]
 
 list_ = [x.drop(columns=['restrictions']) for x in list_]
 
-sch = pd.DataFrame(columns=[*['time'], *list_[0].acronym.unique().tolist()])
 
-pprint(pd.concat([sch, pd.Series(list_[0].time.unique())]))
+def get_member_time(data):
+    sch = pd.DataFrame(columns=[*data.acronym.unique().tolist()])
+
+    sch = pd.concat([pd.Series(data.time.unique()), sch])
+    sch = sch.rename(columns={0: 'time'}).set_index('time')
+
+    for i in data.iterrows():
+        sch.at[i[1].time, i[1].acronym] = i[1].computer
+
+    sch.to_csv('assets/{}}.csv'.format(), encoding='utf-8', index=True)
+
+
+pprint(list(map(get_member_time, list_)))
